485e8d28cc9b0662de6216f00bd04dac6ba48cce
Karthic Kumaran <k3.karthic@live.com>
2021-04-11 13:44:48 +0000

    default security list to only allow internal traffic

--- main.tf
+++ main.tf
@@ -11,12 +11,26 @@ resource "oci_core_vcn" "main" {
   ]
 }
 
+# Only allow internal traffic. Internet traffic controlled by network security groups
+resource "oci_core_security_list" "default" {
+  compartment_id = var.compartment
+  vcn_id         = oci_core_vcn.main.id
+
+  ingress_security_rules {
+    protocol    = "all"
+    source      = "192.168.0.0/16"
+    source_type = "CIDR_BLOCK"
+  }
+}
+
 resource "oci_core_subnet" "private" {
   compartment_id = var.compartment
   vcn_id         = oci_core_vcn.main.id
 
   cidr_block                 = "192.168.254.0/24"
   prohibit_public_ip_on_vnic = true
+
+  security_list_ids = [oci_core_security_list.default.id]
 }
 
 resource "oci_core_internet_gateway" "igw" {
@@ -41,7 +55,9 @@ resource "oci_core_subnet" "public" {
   compartment_id = var.compartment
   vcn_id         = oci_core_vcn.main.id
 
-  cidr_block     = "192.168.1.0/24"
-  dns_label      = "subnet1"
-  route_table_id = oci_core_route_table.public.id
+  cidr_block = "192.168.1.0/24"
+  dns_label  = "subnet1"
+
+  security_list_ids = [oci_core_security_list.default.id]
+  route_table_id    = oci_core_route_table.public.id
 }
